a8bdd062fc292a7f5b54b7cb8dffc6d9a327d79f
Game110AM <Game110AM@605-110-STU19>
2017-04-27 08:34:53 -0600

    commit

---
+++ pokemon0427/pokemon0427/Main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+double Attack(double level, double power, double attack, double defense, double modifier);
+
+int main() {
+
+	double damage = 0;
+	double level = 0;
+	double power = 0;
+	double attack = 0;
+	double defense = 0;
+	double modifier = 0;
+
+	cout << "Enter level" << endl;
+	cin >> level;
+	cout << "Enter power" << endl;
+	cin >> power;
+	cout << "Enter attack" << endl;
+	cin >> attack;
+	cout << "Enter defense" << endl;
+	cin >> defense;
+	cout << "Enter modifier" << endl;
+	cin >> modifier;
+
+	Attack(level, power, attack, defense, modifier);
+
+	cout << Attack(level, power, attack, defense, modifier) << endl;
+
+}
+
+double Attack(double level, double power, double attack, double defense, double modifier) {
+
+	
+	double damage = ((((2 * level / 5 + 2)*power*(attack / defense)) / 50 + 2)*modifier);
+
+	return damage;
+
+}
